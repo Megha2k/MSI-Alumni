77bed40ec6af57bd3a305c1973b00d4465715723
MeghaKhawadia <megha.mask1999@gmail.com>
2020-10-30 01:05:52 +0530

    alumni models

--- alumni_portal/alumni_app/forms.py
+++ alumni_portal/alumni_app/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.db import models
-from alumni_app.models import notice_model 
+from alumni_app.models import notice_model,bca_students_model,bba_students_model,bed_students_model,bcom_students_model
 
 class notice_form(forms.ModelForm): 
          
@@ -11,5 +11,49 @@ class notice_form(forms.ModelForm):
 		labels = {'name':'Name','file':'File'}
 		widgets = {
 					'name':forms.TextInput(attrs={'required':True,'class':'form-control','placeholder':'name of the notice to display','label':"sdfs"}),
+					'file':forms.FileInput(attrs={'required':True,'class':'form-control'})
+				  }
+
+class bca_students_form(forms.ModelForm): 
+         
+	class Meta():
+		model = bca_students_model
+		fields = ['fname','lname','rollno','file']
+
+		labels = {'fname':'First Name','lname':'Last Name','rollno':'Roll Number','file':'File'}
+		widgets = {
+					'file':forms.FileInput(attrs={'required':True,'class':'form-control'})
+				  }
+
+class bba_students_form(forms.ModelForm): 
+         
+	class Meta():
+		model = bba_students_model
+		fields = ['fname','lname','rollno']
+
+		labels = {'fname':'First Name','lname':'Last Name','rollno':'Roll Number','file':'File'}
+		widgets = {
+					'file':forms.FileInput(attrs={'required':True,'class':'form-control'})
+				  }
+
+class bed_students_form(forms.ModelForm): 
+         
+	class Meta():
+		model = bed_students_model
+		fields = ['fname','lname','rollno']
+
+		labels = {'fname':'First Name','lname':'Last Name','rollno':'Roll Number','file':'File'}
+		widgets = {
+					'file':forms.FileInput(attrs={'required':True,'class':'form-control'})
+				  }
+
+class bcom_students_form(forms.ModelForm): 
+         
+	class Meta():
+		model = bcom_students_model
+		fields = ['fname','lname','rollno']
+
+		labels = {'fname':'First Name','lname':'Last Name','rollno':'Roll Number','file':'File'}
+		widgets = {
 					'file':forms.FileInput(attrs={'required':True,'class':'form-control'})
 				  }
--- alumni_portal/static/js/msi_admin.js
+++ alumni_portal/static/js/msi_admin.js
@@ -11,3 +11,55 @@ function notice_show_hide()
 		document.getElementById("notice_show_hide_btn").innerHTML = '+';
 	}
 }
+function bca_students_show_hide()
+{
+	if (document.getElementById("bca_students_show_hide").style.display=="none")
+	{
+		document.getElementById("bca_students_show_hide").style.display="inline";
+		document.getElementById("bca_students_show_hide_btn").innerHTML = '-';
+	}
+	else
+	{
+		document.getElementById("bca_students_show_hide").style.display="none";
+		document.getElementById("bca_students_show_hide_btn").innerHTML = '+';
+	}
+}
+function bba_students_show_hide()
+{
+	if (document.getElementById("bba_students_show_hide").style.display=="none")
+	{
+		document.getElementById("bba_students_show_hide").style.display="inline";
+		document.getElementById("bba_students_show_hide_btn").innerHTML = '-';
+	}
+	else
+	{
+		document.getElementById("bba_students_show_hide").style.display="none";
+		document.getElementById("bba_students_show_hide_btn").innerHTML = '+';
+	}
+}
+function bed_students_show_hide()
+{
+	if (document.getElementById("bed_students_show_hide").style.display=="none")
+	{
+		document.getElementById("bed_students_show_hide").style.display="inline";
+		document.getElementById("bed_students_show_hide_btn").innerHTML = '-';
+	}
+	else
+	{
+		document.getElementById("bed_students_show_hide").style.display="none";
+		document.getElementById("bed_students_show_hide_btn").innerHTML = '+';
+	}
+}
+function bcom_students_show_hide()
+{
+	if (document.getElementById("bcom_students_show_hide").style.display=="none")
+	{
+		document.getElementById("bcom_students_show_hide").style.display="inline";
+		document.getElementById("bcom_students_show_hide_btn").innerHTML = '-';
+	}
+	else
+	{
+		document.getElementById("bcom_students_show_hide").style.display="none";
+		document.getElementById("bcom_students_show_hide_btn").innerHTML = '+';
+	}
+}
