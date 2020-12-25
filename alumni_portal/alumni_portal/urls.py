590109e0ec4430c5775c46cdfe1b8617b3b7adcf
SristiV <sristiv20@gmail.com>
2020-12-25 19:50:39 +0530

    second try for logout

--- alumni_portal/alumni_portal/urls.py
+++ alumni_portal/alumni_portal/urls.py
@@ -20,12 +20,12 @@ from django.conf.urls import include
 from alumni_app import views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'accounts/login/$',views.LoginView.as_view(), name='login'),
+    url(r'accounts/logout/$',views.LogoutView.as_view(), name='logout'),
 
     path('accounts/', include('allauth.urls')), # GOOGLE SIGN IN
     url(r'^alumni_app/', include('alumni_app.urls')),
