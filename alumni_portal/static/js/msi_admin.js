22b7ba2bc8fd3c9bdad1cb26c01516f318f76679
SristiV <sristiv20@gmail.com>
2021-06-21 01:14:33 +0530

    Email Form Shifted to end

--- alumni_portal/static/js/msi_admin.js
+++ alumni_portal/static/js/msi_admin.js
@@ -1,14 +1,17 @@
+// https://stackoverflow.com/questions/6041209/html-character-codes-for-this-or-this
+// Unicode for downward triangle: &#x25BC;
+// Unicode for upward triangle: &#x25B2;
 function notice_show_hide()
 {
 	if (document.getElementById("notice_show_hide").style.display=="none")
 	{
 		document.getElementById("notice_show_hide").style.display="inline";
-		document.getElementById("notice_show_hide_btn").innerHTML = '▼';
+		document.getElementById("notice_show_hide_btn").innerHTML = '&#x25B2;';
 	}
 	else
 	{
 		document.getElementById("notice_show_hide").style.display="none";
-		document.getElementById("notice_show_hide_btn").innerHTML = '▲';
+		document.getElementById("notice_show_hide_btn").innerHTML = '&#x25BC;';
 	}
 }
 function events_show_hide()
@@ -16,12 +19,12 @@ function events_show_hide()
 	if (document.getElementById("events_show_hide").style.display=="none")
 	{
 		document.getElementById("events_show_hide").style.display="inline";
-		document.getElementById("events_show_hide_btn").innerHTML = '▼';
+		document.getElementById("events_show_hide_btn").innerHTML = '&#x25B2;';
 	}
 	else
 	{
 		document.getElementById("events_show_hide").style.display="none";
-		document.getElementById("events_show_hide_btn").innerHTML = '▲';
+		document.getElementById("events_show_hide_btn").innerHTML = '&#x25BC;';
 	}
 }
 function slideshow_show_hide()
@@ -29,12 +32,12 @@ function slideshow_show_hide()
 	if (document.getElementById("slideshow_show_hide").style.display=="none")
 	{
 		document.getElementById("slideshow_show_hide").style.display="inline";
-		document.getElementById("slideshow_show_hide_btn").innerHTML = '▼';
+		document.getElementById("slideshow_show_hide_btn").innerHTML = '&#x25B2;';
 	}
 	else
 	{
 		document.getElementById("slideshow_show_hide").style.display="none";
-		document.getElementById("slideshow_show_hide_btn").innerHTML = '▲';
+		document.getElementById("slideshow_show_hide_btn").innerHTML = '&#x25BC;';
 	}
 }
 function display_alumni_show_hide()
@@ -42,12 +45,12 @@ function display_alumni_show_hide()
 	if (document.getElementById("display_alumni_show_hide").style.display=="none")
 	{
 		document.getElementById("display_alumni_show_hide").style.display="inline";
-		document.getElementById("display_alumni_show_hide_btn").innerHTML = '▼';
+		document.getElementById("display_alumni_show_hide_btn").innerHTML = '&#x25B2;';
 	}
 	else
 	{
 		document.getElementById("display_alumni_show_hide").style.display="none";
-		document.getElementById("display_alumni_show_hide_btn").innerHTML = '▲';
+		document.getElementById("display_alumni_show_hide_btn").innerHTML = '&#x25BC;';
 	}
 }
 function placement_companies_show_hide()
@@ -55,12 +58,12 @@ function placement_companies_show_hide()
 	if (document.getElementById("placement_companies_show_hide").style.display=="none")
 	{
 		document.getElementById("placement_companies_show_hide").style.display="inline";
-		document.getElementById("placement_companies_show_hide_btn").innerHTML = '▼';
+		document.getElementById("placement_companies_show_hide_btn").innerHTML = '&#x25B2;';
 	}
 	else
 	{
 		document.getElementById("placement_companies_show_hide").style.display="none";
-		document.getElementById("placement_companies_show_hide_btn").innerHTML = '▲';
+		document.getElementById("placement_companies_show_hide_btn").innerHTML = '&#x25BC;';
 	}
 }
 function grant_achievements_show_hide()
@@ -68,12 +71,12 @@ function grant_achievements_show_hide()
 	if (document.getElementById("grant_achievements_show_hide").style.display=="none")
 	{
 		document.getElementById("grant_achievements_show_hide").style.display="inline";
-		document.getElementById("grant_achievements_show_hide_btn").innerHTML = '▼';
+		document.getElementById("grant_achievements_show_hide_btn").innerHTML = '&#x25B2;';
 	}
 	else
 	{
 		document.getElementById("grant_achievements_show_hide").style.display="none";
-		document.getElementById("grant_achievements_show_hide_btn").innerHTML = '▲';
+		document.getElementById("grant_achievements_show_hide_btn").innerHTML = '&#x25BC;';
 	}
 }
 function bca_students_show_hide()
@@ -81,12 +84,12 @@ function bca_students_show_hide()
 	if (document.getElementById("bca_students_show_hide").style.display=="none")
 	{
 		document.getElementById("bca_students_show_hide").style.display="inline";
-		document.getElementById("bca_students_show_hide_btn").innerHTML = '▼';
+		document.getElementById("bca_students_show_hide_btn").innerHTML = '&#x25B2;';
 	}
 	else
 	{
 		document.getElementById("bca_students_show_hide").style.display="none";
-		document.getElementById("bca_students_show_hide_btn").innerHTML = '▲';
+		document.getElementById("bca_students_show_hide_btn").innerHTML = '&#x25BC;';
 	}
 }
 function bba_students_show_hide()
@@ -94,12 +97,12 @@ function bba_students_show_hide()
 	if (document.getElementById("bba_students_show_hide").style.display=="none")
 	{
 		document.getElementById("bba_students_show_hide").style.display="inline";
-		document.getElementById("bba_students_show_hide_btn").innerHTML = '▼';
+		document.getElementById("bba_students_show_hide_btn").innerHTML = '&#x25B2;';
 	}
 	else
 	{
 		document.getElementById("bba_students_show_hide").style.display="none";
-		document.getElementById("bba_students_show_hide_btn").innerHTML = '▲';
+		document.getElementById("bba_students_show_hide_btn").innerHTML = '&#x25BC;';
 	}
 }
 function bed_students_show_hide()
@@ -107,12 +110,12 @@ function bed_students_show_hide()
 	if (document.getElementById("bed_students_show_hide").style.display=="none")
 	{
 		document.getElementById("bed_students_show_hide").style.display="inline";
-		document.getElementById("bed_students_show_hide_btn").innerHTML = '▼';
+		document.getElementById("bed_students_show_hide_btn").innerHTML = '&#x25B2;';
 	}
 	else
 	{
 		document.getElementById("bed_students_show_hide").style.display="none";
-		document.getElementById("bed_students_show_hide_btn").innerHTML = '▲';
+		document.getElementById("bed_students_show_hide_btn").innerHTML = '&#x25BC;';
 	}
 }
 function bcom_students_show_hide()
@@ -120,11 +123,11 @@ function bcom_students_show_hide()
 	if (document.getElementById("bcom_students_show_hide").style.display=="none")
 	{
 		document.getElementById("bcom_students_show_hide").style.display="inline";
-		document.getElementById("bcom_students_show_hide_btn").innerHTML = '▼';
+		document.getElementById("bcom_students_show_hide_btn").innerHTML = '&#x25B2;';
 	}
 	else
 	{
 		document.getElementById("bcom_students_show_hide").style.display="none";
-		document.getElementById("bcom_students_show_hide_btn").innerHTML = '▲';
+		document.getElementById("bcom_students_show_hide_btn").innerHTML = '&#x25BC;';
 	}
 }
