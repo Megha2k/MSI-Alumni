c0de147fdfef271833eeb7ed71e7b9ec45c289b0
SristiV <sristiv20@gmail.com>
2021-06-23 20:03:33 +0530

    Removed changes made to admin page

--- alumni_portal/chat/admin.py
+++ alumni_portal/chat/admin.py
@@ -2,9 +2,5 @@ from django.contrib import admin
 from chat.models import Message
 
 # Register your models here.
-class MessageAdmin(admin.ModelAdmin):
-	list_display = ["room","username","content","date_added"]
-	search_fields = ["room","username",'date_added']
-	list_filter = ['room','date_added']
 
-admin.site.register(Message, MessageAdmin)
+admin.site.register(Message)
