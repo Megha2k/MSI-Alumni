a6ce390ff6aee75f7210ae72fd048e815d19acfe
SristiV <67922098+SristiV@users.noreply.github.com>
2023-08-15 22:19:28 +0530

    Update urls.py url is replaced with re_path(for django version4)

--- alumni_portal/alumni_app/urls.py
+++ alumni_portal/alumni_app/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import include, re_path
 from alumni_app import views
 from django.urls import path
 
 app_name= 'alumni_app'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    re_path(r'^$', views.index, name='index'),
     path('about',views.about,name='about'),
 ]
