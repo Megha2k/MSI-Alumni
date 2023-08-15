7e19274e4db2cd59f7b5fc564e28d40490db6b3c
SristiV <67922098+SristiV@users.noreply.github.com>
2023-08-15 22:17:53 +0530

    Update urls.py for django version 4

--- alumni_portal/alumni_portal/urls.py
+++ alumni_portal/alumni_portal/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url
-from django.conf.urls import include
+from django.urls import include, re_path
 from alumni_app import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -11,8 +10,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')), # GOOGLE SIGN IN
     path('chat/', include('chat.urls'),name='chat'), #For Chat
-    url(r'^alumni_app/', include('alumni_app.urls')),
-    url(r'^$', views.index, name='index'),
+    re_path(r'^alumni_app/', include('alumni_app.urls')),
+    re_path(r'^$', views.index, name='index'),
     path('bca_placement/', views.bca_placement, name='bca_placement'),
     path('bba_placement/', views.bba_placement, name='bba_placement'),
     path('bed_placement/', views.bed_placement, name='bed_placement'),
@@ -23,9 +22,9 @@ urlpatterns = [
     path('login_success/', views.login_success, name='login_success'),
     path('msi_admin/', views.msi_admin, name='msi_admin'),
     path('alumni/', views.alumni, name='alumni'),
-    url(r'^homepage',views.index,name='homepage'),
+    re_path(r'^homepage',views.index,name='homepage'),
     path('send/', views.sendanemail, name="sendemail"),
-    url(r'^gallery',views.gallery,name="gallery"),
-    url(r'^allAlumni',views.AlumniListView.as_view(),name="allAlumni"),
+    re_path(r'^gallery',views.gallery,name="gallery"),
+    re_path(r'^allAlumni',views.AlumniListView.as_view(),name="allAlumni"),
     path('alumni/<int:pk>', views.AlumniDetailView.as_view(), name='alumniDetail'),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
